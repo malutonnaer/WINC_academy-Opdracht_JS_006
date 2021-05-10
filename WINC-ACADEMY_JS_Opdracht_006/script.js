e4fe5276197e2ca24dc7c2cc3bad0709a9fd86bc
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-10 14:13:17 +0200

    Add files via upload

---
+++ WINC-ACADEMY_JS_Opdracht_006/script.js
@@ -0,0 +1,16 @@
+const makePizza = function () {
+    console.log("Make pizza dough");
+    console.log("Make sauce");
+    console.log("Put sauce on dough");
+    console.log("Heat 12 min in 300deg oven");
+};
+
+makePizza(2);
+
+const makeSushi = function () {
+    console.log("Boil rice");
+    console.log("Cut fish");
+    console.log("Roll rice and fish in seeweed");
+};
+
+makeSushi(3);
